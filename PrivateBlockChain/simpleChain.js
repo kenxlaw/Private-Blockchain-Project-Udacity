e8aa0d00a135f5b49190cb50960126c3bc0f7305
kenxlaw <kenxlaw7@gmail.com>
2022-04-19 07:44:41 +0100

    Added Height and Timestamp

--- PrivateBlockChain/simpleChain.js
+++ PrivateBlockChain/simpleChain.js
@@ -19,6 +19,8 @@ class Blockchain {
         return new Block("First block in the chain - Genesis block");
     }
     addBlock(newBlock) {
+        newBlock.height = this.chain.length;
+        newBlock.time = new Date().getTime().toString().slice(0.-3);
         if (this.chain.length > 0) {
             newBlock.previousBlockHash = this.chain[this.chain.length-1].hash;
             }
